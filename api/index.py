efd866e6c709237597314aaaf845160c024e43dc
Aditya Sinha <adityasinhauw@gmail.com>
2021-01-25 01:28:07 -0800

    added top artists for all 3 time ranges

--- api/index.py
+++ api/index.py
@@ -1,7 +1,6 @@
 import os
-
 import requests
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, make_response
 
 CLIENT_ID = os.environ.get('CLIENT_ID')
 CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
@@ -23,7 +22,10 @@ def callfrom():
 
 @app.route('/login', methods=['GET'])
 def login():
-    code = request.args['code']
+    try:
+        code = request.args['code']
+    except KeyError:
+        return redirect('/callfrom')
 
     url = 'https://accounts.spotify.com/api/token'
     payload = {
@@ -37,6 +39,17 @@ def login():
     response = response.json()
     refresh_token = response['refresh_token']
     access_token = response['access_token']
+
+    resp = make_response(redirect('/dashboard'))
+    resp.set_cookie('access_token', access_token)
+    return resp
+
+
+@app.route('/dashboard')
+def dashboard():
+    access_token = request.cookies.get('access_token')
+    if not access_token:
+        return redirect('/login')
 
     url = "https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=10&offset=0"
     payload = {}
@@ -99,13 +112,11 @@ def login():
     response = requests.request("GET", url, headers=headers, data=payload)
     long_term_artists = response.json()
 
+    # return render_template('test.html')
+    print(short_term_tracks)
     return render_template('name.html', short_term_tracks=short_term_tracks, medium_term_tracks=medium_term_tracks,
                            long_term_tracks=long_term_tracks, short_term_artists=short_term_artists,
                            medium_term_artists=medium_term_artists, long_term_artists=long_term_artists)
-
-
-# @app.route('/dashboard')
-
 
 if __name__ == "__main__":
     app.run(debug=True)
